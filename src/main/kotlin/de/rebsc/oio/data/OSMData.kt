5120161b69e2f66b031f3fe9d2d6d4c20c5ac5ed
rebeccasc <65017227+rebeccasc@users.noreply.github.com>
2021-06-01 18:24:00 +0200

    Implement intersection method for ways

--- src/main/kotlin/de/rebsc/oio/data/OSMData.kt
+++ src/main/kotlin/de/rebsc/oio/data/OSMData.kt
@@ -1,5 +1,9 @@
 package de.rebsc.oio.data
 
+import org.locationtech.jts.geom.Coordinate
+import org.locationtech.jts.geom.GeometryFactory
+
+
 /******************************************************************************
  * Copyright (C) 2021  de.rebsc
  *
@@ -49,13 +53,40 @@ data class OSMNode(val id: Long, val x: Double, val y: Double, val tags: List<OS
 /**
  * Way holding nodes and tags
  */
-data class OSMWay(val id: Long, val points: ArrayList<OSMNode>, val tags: ArrayList<OSMTag>) {
+class OSMWay(val id: Long, val points: ArrayList<OSMNode>, val tags: ArrayList<OSMTag>) {
 
     constructor(id: Long, nodes: ArrayList<OSMNode>) : this(id, nodes, ArrayList<OSMTag>())
     constructor(id: Long) : this(id, ArrayList<OSMNode>(), ArrayList<OSMTag>())
+
+    /**
+     * Gets intersection points of this and [way]
+     * @param way to get intersections with
+     * @return array with intersection points, empty if none
+     */
+    fun intersects(way: OSMWay): ArrayList<Point2D> {
+        val coord1 = arrayOfNulls<Coordinate>(this.points.size)
+        for (i in 0 until this.points.size) {
+            coord1[i] = Coordinate(this.points[i].x, this.points[i].y)
+        }
+        val coord2 = arrayOfNulls<Coordinate>(way.points.size)
+        for (i in 0 until way.points.size) {
+            coord2[i] = Coordinate(way.points[i].x, way.points[i].y)
+        }
+        val poly1 = GeometryFactory().createLinearRing(coord1)
+        val poly2 = GeometryFactory().createLinearRing(coord2)
+
+        val intersectingPoints = poly1.intersection(poly2)
+
+        val intersects = ArrayList<Point2D>()
+        intersectingPoints.coordinates.forEach { c ->
+            intersects.add(Point2D(c.x, c.y))
+        }
+        return intersects
+    }
 }
 
 /**
  * Object tag with key, value pair
  */
 data class OSMTag(val k: String, val v: String)
+
